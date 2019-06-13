53c766f4dda47f2c979afb83a125501befe6c334
Anonymous <user@rivne.itstep.org>
2019-06-13 20:22:16 +0300

    second commit

--- src/components/animal/index.js
+++ src/components/animal/index.js
@@ -4,15 +4,24 @@ import * as animalActions from './reducer';
 import get from 'lodash.get';
 
 class AnimalWidgetContainer extends Component {
-    state = {  }
+    state = {}
     render() {
-        console.log('===Props-===',this.props);
-        return ( 
+        console.log('===Props-===', this.props);
+        return (
             <div>
                 <h1>Hello Animal</h1>
             </div>
-         );
+        );
     }
 }
+const mapState = (state) => {
+    return {
+        list: get(state, 'animal.list.data'),
+        isListLoading: get(state, 'animal.list.loading'),
+        isListError: get(state, 'animal.list.error')
+    }
+}
+const AnimalWidget = connect(mapState)(AnimalWidgetContainer);
 const AnimalWidget = connect()(AnimalWidgetContainer);
+
 export default AnimalWidget;
